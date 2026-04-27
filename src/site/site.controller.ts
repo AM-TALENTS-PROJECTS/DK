7b7c34d6f4579d0c689a6b15005f57eeb0b828a5
Roman Goll <loxklm04@gmail.com>
2026-04-27 17:03:33 +0200

    pitié

--- src/site/site.controller.ts
+++ src/site/site.controller.ts
@@ -27,6 +27,36 @@ export class SiteController {
     res.sendFile(path.join(this.frontendDir, 'cgv.html'));
   }
 
+  @Get('/galerie')
+  galerie(@Res() res: Response): void {
+    res.sendFile(path.join(this.frontendDir, 'galerie.html'));
+  }
+
+  @Get('/nos-prestations')
+  nosPrestations(@Res() res: Response): void {
+    res.sendFile(path.join(this.frontendDir, 'nos-prestations.html'));
+  }
+
+  @Get('/notre-histoire')
+  notreHistoire(@Res() res: Response): void {
+    res.sendFile(path.join(this.frontendDir, 'notre-histoire.html'));
+  }
+
+  @Get('/avis')
+  avis(@Res() res: Response): void {
+    res.sendFile(path.join(this.frontendDir, 'avis.html'));
+  }
+
+  @Get('/contact')
+  contact(@Res() res: Response): void {
+    res.sendFile(path.join(this.frontendDir, 'contact.html'));
+  }
+
+  @Get('/404')
+  notFound(@Res() res: Response): void {
+    res.sendFile(path.join(this.frontendDir, '404.html'));
+  }
+
   @Get('/sitemap.xml')
   sitemap(@Res() res: Response): void {
     res.sendFile(resolveRootFile('sitemap.xml'));
